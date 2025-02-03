94b527e80e0486bbb95b7d9d09a800ed90b338b0
TanisshaDash <tanissha.1610@gmail.com>
2025-02-03 20:18:05 +0530

    updates

--- todoapp.py
+++ todoapp.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 import os 
 
 load_dotenv()
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
