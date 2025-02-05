2a41800544a681c9e1a8de7783befa3fa040d36c
TanisshaDash <tanissha.1610@gmail.com>
2025-02-05 21:05:20 +0530

    todo list

--- TodoList/todoapp.py
+++ TodoList/todoapp.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os 
 
+
 load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('TODO_DB_URL')
