afbd402f93b4e4a6d0407143db5087c8dbfe7079
TanisshaDash <tanissha.1610@gmail.com>
2025-08-27 14:12:26 +0530

    Guess Number Game

--- Guess_the_Number/app.py
+++ Guess_the_Number/app.py
@@ -29,8 +29,9 @@ jwt = JWTManager(app)
 
 # --- Models ---
 class User(db.Model):
+    __tablename__ = "user"   # this sets the table name
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
 
